b3ce937135be8233a3d766497aa8a37b2f28a8a5
MarvinMendes <marvin.m.lopes@gmail.com>
2025-02-23 19:25:25 -0300

    Corrigindo os testes dos servicos de transacao

--- api-gestao-bancaria/src/test/java/com/example/demo/TransacaoTest.java
+++ api-gestao-bancaria/src/test/java/com/example/demo/TransacaoTest.java
@@ -3,31 +3,26 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.util.ObjectUtils;
 
-import com.example.demo.exceptions.CampoNuloNaoPermitidoException;
-import com.example.demo.exceptions.ContaInexistentesException;
-import com.example.demo.exceptions.NumeroNegativoException;
 import com.example.demo.exceptions.SaldoInsuficienteException;
-import com.example.demo.exceptions.TipoNaoPermitidoException;
 import com.example.demo.model.Conta;
 import com.example.demo.model.Transacao;
-import com.example.demo.model.dto.RequisicaoCriacaoContaDto;
 import com.example.demo.model.dto.RequisicaoTransacaoDto;
-import com.example.demo.repository.ContaRepository;
 import com.example.demo.repository.TransacaoRepository;
 import com.example.demo.service.ContaService;
 import com.example.demo.service.TransacaoService;
@@ -35,105 +30,65 @@ import com.example.demo.service.strategy.TransacaoStrategy;
 
 public class TransacaoTest {
 
-		String PIX = "P";
-		String DEBITO = "D";
-		String CREDITO = "C";
-	
-		@InjectMocks
-	    private ContaService contaService; 
+	@InjectMocks
+	private TransacaoService transacaoService;
+
+	@Mock
+	private ContaService contaService;
+
+	@Mock
+	private TransacaoRepository repository;
+
+	private Map<String, TransacaoStrategy> transacaoMap;
+
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
+		transacaoMap = new HashMap<>();
+		transacaoMap.put("C", mock(TransacaoStrategy.class));
 		
-		@InjectMocks
-		private TransacaoService transacaoService;
+	}
 
-	    @Mock
-	    private TransacaoRepository repository; 
-	    
-	    @Mock
-	    private ContaRepository contaRepository;
-	    
-	    @Mock
-	    private TransacaoStrategy transacaoStrategy;
+	@Test
+	public void testChamaStrategia() {
+		RequisicaoTransacaoDto dto = new RequisicaoTransacaoDto();
+		dto.setNumConta(1L);
+		dto.setTipo("C");
+		dto.setValor(new BigDecimal("100.00"));
 
-	    @BeforeEach
-	    public void setUp() {
-	        MockitoAnnotations.openMocks(this); 
-	    }
-	    
-	    
-	    @Test
-	    public void testChamaStrategia() {
-	        RequisicaoTransacaoDto dto = new RequisicaoTransacaoDto();
-	        dto.setNumConta(1L);
-	        dto.setTipo("C"); 
-	        dto.setValor(new BigDecimal("100.00"));
+		Conta conta = new Conta(1L, new BigDecimal("200.00"));
 
-	        Conta conta = new Conta(1L, new BigDecimal("200.00")); 
-	        
-	        RequisicaoCriacaoContaDto novaConta = new RequisicaoCriacaoContaDto(1L, new BigDecimal("200.00"));
+		when(contaService.buscaPorNumero(dto.getNumConta())).thenReturn(conta);
 
-	        contaService.salvar(novaConta);
-	        
-	        when(contaService.buscaPorNumero(1L)).thenReturn(conta);
-	        
-	        when(transacaoStrategy.validaTransacao(novaConta.getSaldoInicial(), dto.getValor())).thenReturn(dto.getValor());
+		when(transacaoMap.get(dto.getTipo()).validaTransacao(conta.getSaldo(), dto.getValor()))
+		.thenReturn(dto.getValor());
 
-	        transacaoService.chamaStrategia(dto);
+		transacaoService.chamaStrategia(dto);
 
-	        assertEquals(new BigDecimal("100.00"), novaConta.getSaldoInicial()); 
-	        verify(repository, times(1)).save(any(Transacao.class)); 
-	    }
-	    
-	    
-	    
-	    @Test
-	    public void testChamaStrategiaComSaldoInsuficiente() {
-	        RequisicaoTransacaoDto dto = new RequisicaoTransacaoDto();
-	        dto.setNumConta(1L);
-	        dto.setTipo("C");
-	        dto.setValor(new BigDecimal("300.00")); 
+		assertEquals(new BigDecimal("97.0000"), conta.getSaldo());
+		verify(repository, times(1)).save(any(Transacao.class)); 
+	}
 
-	        Conta conta = new Conta(1L, new BigDecimal("200.00"));
+	@Test
+	public void testChamaStrategiaComSaldoInsuficiente() {
+		RequisicaoTransacaoDto dto = new RequisicaoTransacaoDto();
+		dto.setNumConta(1L);
+		dto.setTipo("C");
+		dto.setValor(new BigDecimal("300.00")); 
 
-	        contaRepository.save(conta);
-	        
-	        when(buscaPorNumero(dto.getNumConta())).thenReturn(conta);
-	        
-	        when(transacaoStrategy.validaTransacao(conta.getSaldo(), dto.getValor()))
-	        .thenThrow(new SaldoInsuficienteException(new BigDecimal("200.00")));
+		Conta conta = new Conta(1L, new BigDecimal("200.00"));
 
-	        SaldoInsuficienteException exception = assertThrows(SaldoInsuficienteException.class, () -> {
-	            transacaoService.chamaStrategia(dto);
-	        });
+		when(contaService.buscaPorNumero(dto.getNumConta())).thenReturn(conta);
 
-	        assertEquals("Saldo insuficiente", exception.getMessage());
-	        verify(repository, never()).save(any(Transacao.class)); 
-	    }
-	    
-	    public Conta buscaPorNumero(Long conta) {
-			if(contaRepository.findById(conta).isPresent()) {
-				return contaRepository.findById(conta).get();			
-			}
-			throw new ContaInexistentesException() ;
-			
-		}
-	    
-	    private void validaCamposDaTransacao(RequisicaoTransacaoDto dto) {
-			
-			if(ObjectUtils.isEmpty(dto.getNumConta()) || ObjectUtils.isEmpty(dto.getTipo().toUpperCase()) || ObjectUtils.isEmpty(dto.getValor())) {
-				throw new CampoNuloNaoPermitidoException();
-			}
-			
-			
-			int valor = dto.getValor().compareTo(BigDecimal.ZERO);
-			if(dto.getNumConta() < 0 || valor < 0) {
-				throw new NumeroNegativoException();
-			}
-			
-			List<String> tiposPermitidos = List.of(CREDITO, DEBITO,PIX);
-			if(!tiposPermitidos.contains(dto.getTipo().toUpperCase())) {
-				throw new TipoNaoPermitidoException();
-			}
-			
-		}
-	
+		when(transacaoMap.get(dto.getTipo()).validaTransacao(conta.getSaldo(), dto.getValor()))
+		.thenThrow(new SaldoInsuficienteException(new BigDecimal("200.00")));
+
+		SaldoInsuficienteException exception = assertThrows(SaldoInsuficienteException.class, () -> {
+			transacaoService.chamaStrategia(dto);
+		});
+
+		assertEquals("Saldo insuficiente. O saldo disponível é de: R$ ".concat("200.00"), exception.getMessage());
+		verify(repository, never()).save(any(Transacao.class));
+	}
+
 }
